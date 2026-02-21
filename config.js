22c3f8ec3356994710283f066efa6fa4cc05a124
freeze-beep <freezec852@gmail.com>
2026-02-22 00:29:39 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    ownerNumber: "243986860268@s.whatsapp.net",
+    pairingNumber: "243986860268",
+    botName: "AYANOKOJI-BOT",
+    ownerName: "Kiyotaka Ayanokoji",
+    prefix: ".",
+    imageMenu: "https://files.catbox.moe/9f9p3p.jpg",
+    mode: "private" // Le bot ne répondra qu'à toi
+};
+
+module.exports = config;
